36e3d573d26483308db0444976421bb58156b77d
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-05-09 01:06:33 +0600

    contact

--- js/script.js
+++ js/script.js
@@ -1,37 +1,77 @@
-
-let dates = document.getElementById('date');
+let dates = document.getElementById("date");
 
 setInterval(() => {
-    let date = new Date()
-    dates.innerHTML=date.toLocaleString('en')
+  let date = new Date();
+  dates.innerHTML = date.toLocaleString("en");
 }, 1000);
 
+// let name = document.getElementById('nameInpute');
+// let email = document.getElementById('emailInpute');
+// let password = document.getElementById('passwordInpute');
+// let address = document.getElementById('addressInpute');
+// let textaria = document.getElementById('textaria');
+// let subBtn = document.getElementById('subBtn');
+
+// subBtn.addEventListener('click',()=>{
+//     if (name.value === "") {
+//         alert('Enter Your Name')
+//     }
+//     else if (email.value === "") {
+//         alert('Enter Your Email')
+//     }
+//     else if (password.value === "") {
+//         alert('Enter Your Password')
+//     }
+//     else if (address.value === "") {
+//         alert('Enter Your Address')
+//     }
+//     else if (textaria.value === "") {
+//         alert('Enter Your Text')
+//     }
+//     else{
+//         alert('Susscess')
+//     }
+// })
 
 
-let name = document.getElementById('nameInpute');
-let email = document.getElementById('emailInpute');
-let password = document.getElementById('passwordInpute');
-let address = document.getElementById('addressInpute');
-let textaria = document.getElementById('textaria');
-let subBtn = document.getElementById('subBtn');
 
-subBtn.addEventListener('click',()=>{
-    if (name.value === "") {
-        alert('Enter Your Name')
+ let nameInpute = document.getElementById('nameInpute');
+ let emailInpute = document.getElementById('emailInpute');
+ let passwordInpute = document.getElementById('passwordInpute');
+ let addressInpute = document.getElementById('addressInpute');
+ let textariaInpute = document.getElementById('textariaInpute');
+
+let names = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let address = document.getElementById("address");
+let textaria = document.getElementById("textaria");
+let subBtn = document.getElementById("subBtn");
+
+subBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    names.setAttribute('hidden',true)
+    email.setAttribute('hidden',true)
+    password.setAttribute('hidden',true)
+    address.setAttribute('hidden',true)
+    textaria.setAttribute('hidden',true)
+
+    if (nameInpute.value === "") {
+        names.removeAttribute('hidden')
     }
-    else if (email.value === "") {
-        alert('Enter Your Email')
+    else if (emailInpute.value === "") {
+        email.removeAttribute('hidden')
     }
-    else if (password.value === "") {
-        alert('Enter Your Password')
+    else if (passwordInpute.value === "") {
+        password.removeAttribute('hidden')
     }
-    else if (address.value === "") {
-        alert('Enter Your Address')
+    else if (addressInpute.value === "") {
+        address.removeAttribute('hidden')
     }
-    else if (textaria.value === "") {
-        alert('Enter Your Text')
+    else if (textariaInpute.value === "") {
+        textaria.removeAttribute('hidden')
     }
     else{
-        alert('Susscess')
+        alert('Submit')
     }
 })
